f4fb54c17ccc7597603c3517f72493af94fec838
Marat Galiev <kazanlug@gmail.com>
2020-05-01 02:28:59 -0400

    Added company info

--- bin/index.js
+++ bin/index.js
@@ -3,6 +3,8 @@
 const boxen = require("boxen");
 const chalk = require("chalk");
 const yargs = require("yargs");
+const text_wrapper_lib = require('text-wrapper');
+const wrapper = text_wrapper_lib.wrapper;
 var yahooFinance = require('yahoo-finance');
 var _ = require('lodash');
 
@@ -10,16 +12,19 @@ const colors = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white', 'g
 
 const selColor = colors[Math.floor(Math.random() * colors.length)];
 
+
 const options = yargs
  .usage("Usage: -s <symbol1 symbol2>")
  .option("s", { alias: "symbols", describe: "Your symbols", type: "array", demandOption: true })
  .option("c", { alias: "color", describe: "Border color", type: "string", demandOption: false })
+ .option("i", { alias: "info", describe: "Company info", type: "boolean", demandOption: false })
  .argv;
 
 const symbols = options.symbols;
 const argsColor = options.color;
+const showInfo = options.info;
 
-const boxenOptions = {
+const symbolsBoxenOptions = {
   padding: 1,
   margin: 1,
   bold: true,
@@ -27,21 +32,41 @@ const boxenOptions = {
   borderColor: argsColor ? argsColor : selColor
 };
 
+const infoBoxenOtions = {
+  padding: 1,
+  margin: 1,
+  bold: false,
+  borderStyle: "classic",
+  borderColor: argsColor ? argsColor : selColor
+};
+
 yahooFinance.quote({
   symbols: symbols,
-  modules: [ 'price' ]
+  modules: [ 'price', 'summaryProfile' ]
 }, function (err, quotes) {
   if (err) { console.log('Error while doing request, check your params') }
   let data = new Array();
+  let info = new Array();
   _.each(quotes, function (quote, symbol) {
     if (quote.price.regularMarketPrice) {
       const greeting = `${chalk.white.bold.underline(symbol)}: ${chalk.white.bold(quote.price.regularMarketPrice)} ${(quote.price.regularMarketChange).toFixed(2)} (${(quote.price.regularMarketChangePercent * 100).toFixed(2)}%)`;
+      const companyInfo = `${chalk.white.bold.underline(symbol)}: ${quote.summaryProfile.longBusinessSummary}`;
       if (greeting) {
         data.push(greeting);
       }
+      if (showInfo && companyInfo) {
+        info.push(companyInfo);
+        info.push('\n\n');
+      }
     }
   });
-  const msgBox = boxen(data.join('\n'), boxenOptions);
-  console.log(msgBox);
+  
+  const symbolsBox = boxen(data.join('\n'), symbolsBoxenOptions);
+  console.log(symbolsBox);
+
+  if(showInfo) {
+    const infoBox = boxen(wrapper(info.join('\n')), infoBoxenOtions);
+    console.log(infoBox);
+  }
 });
 
