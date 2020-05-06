097a070fab00d06c9882942325586761700f1782
Marat Galiev <kazanlug@gmail.com>
2020-05-06 13:38:48 -0400

    Average param added

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+const clear = require('clear');
+const figlet = require('figlet');
 const boxen = require("boxen");
 const chalk = require("chalk");
 const yargs = require("yargs");
@@ -12,10 +14,10 @@ const colors = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white', 'g
 
 const selColor = colors[Math.floor(Math.random() * colors.length)];
 
-
 const options = yargs
  .usage("Usage: -s <symbol1 symbol2>")
  .option("s", { alias: "symbols", describe: "Your symbols", type: "array", demandOption: true })
+ .option("a", { alias: "average", describe: "200 days average", type: "boolean", demandOption: false })
  .option("c", { alias: "color", describe: "Border color", type: "string", demandOption: false })
  .option("i", { alias: "info", describe: "Company info", type: "boolean", demandOption: false })
  .argv;
@@ -23,6 +25,7 @@ const options = yargs
 const symbols = options.symbols;
 const argsColor = options.color;
 const showInfo = options.info;
+const showAverage = options.average;
 
 const symbolsBoxenOptions = {
   padding: 1,
@@ -40,16 +43,28 @@ const infoBoxenOtions = {
   borderColor: argsColor ? argsColor : selColor
 };
 
+clear();
+
+console.log(
+  chalk.yellow(
+    figlet.textSync('STOCKY', { horizontalLayout: 'full' })
+  )
+);
+
 yahooFinance.quote({
   symbols: symbols,
-  modules: [ 'price', 'summaryProfile' ]
+  modules: [ 'price', 'summaryProfile', 'summaryDetail']
 }, function (err, quotes) {
   if (err) { console.log('Error while doing request, check your params') }
   let data = new Array();
   let info = new Array();
+  let average = null;
   _.each(quotes, function (quote, symbol) {
     if (quote.price.regularMarketPrice) {
-      const greeting = `${chalk.white.bold.underline(symbol)}: ${chalk.white.bold(quote.price.regularMarketPrice)} ${(quote.price.regularMarketChange).toFixed(2)} (${(quote.price.regularMarketChangePercent * 100).toFixed(2)}%)`;
+      let greeting = `${chalk.white.bold.underline(symbol)}: ${chalk.white.bold(quote.price.regularMarketPrice)} ${(quote.price.regularMarketChange).toFixed(2)} (${(quote.price.regularMarketChangePercent * 100).toFixed(2)}%)`;
+      if (showAverage && quote.summaryDetail) {
+        greeting += ` | 200 DAYS AVERAGE: ${chalk.white.bold.underline(quote.summaryDetail.twoHundredDayAverage.toFixed(2))}`
+      }
       const companyInfo = `${chalk.white.bold.underline(symbol)}: ${quote.summaryProfile.longBusinessSummary}`;
       if (greeting) {
         data.push(greeting);
